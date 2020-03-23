1dfee505ee9af7a43665b2a96c8f505ad0c271d7
quarryman <quarrying@qq.com>
2020-03-23 23:37:37 +0800

    update scripts/get_record_number.py

--- scripts/get_record_number.py
+++ scripts/get_record_number.py
@@ -36,6 +36,21 @@ def get_record_number(records):
     return number
 
 
+def get_record_number_with_desc(records):
+    number = 0
+    for record in records:
+        record = record.strip()
+        if record == '':
+            pass
+        elif record.startswith('#'):
+            pass
+        else:
+            parts = record.split(',')
+            if len(parts) > 1 and len(parts[1]) > 1:
+                number += 1
+    return number
+
+
 if __name__ == '__main__':
     src_dir = r'F:\_Datasets_self\CelebNames\names'
     tags = ['businessman_chinese', 'businessman_japan_korea', 'businessman_others', 
@@ -43,13 +58,13 @@ if __name__ == '__main__':
             'politican_chinese', 'politican_japan_korea_vn_sg', 'politican_others',
             'scientist_chinese', 'scientist_others',
             'star_chinese', 'star_japan_korea', 'star_others']
-    total = 0
+    all_records = []
     for tag in tags:
         filenames = glob.glob(os.path.join(src_dir, '{}*.txt'.format(tag)))
         records = get_all_records(filenames)
         print('{:<30}: {}'.format(tag, get_record_number(records)))
-        total += get_record_number(records)
-    print('{:<30}: {}'.format('total', total))
-    
+        all_records += records
+    print('{:<30}: {}'.format('total', get_record_number(all_records)))
+    print('{:<30}: {}'.format('total with desc', get_record_number_with_desc(all_records)))
     
     
